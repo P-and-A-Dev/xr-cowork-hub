02e565e5bb3191788522a59312f003d421ec6ed6
Antonin Do Souto <avainfoconseils@gmail.com>
2025-12-05 02:33:18 +0100

    fix: fix error on firebase init

--- unity-client/XR Cowork Hub/Assets/_Project/Scripts/Manager/FirebaseInit.cs
+++ unity-client/XR Cowork Hub/Assets/_Project/Scripts/Manager/FirebaseInit.cs
@@ -1,7 +1,6 @@
+using System;
 using Firebase;
-using Firebase.Auth;
 using Firebase.Extensions;
-using Firebase.Firestore;
 using UnityEngine;
 
 namespace _Project.Scripts.Manager
@@ -22,52 +21,8 @@ namespace _Project.Scripts.Manager
                 }
                 else
                 {
-                    Debug.LogError(System.String.Format(
-                        "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                    Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
                 }
-            });
-#if UNITY_EDITOR
-            // Skip Firebase in Editor to avoid crashes
-            Debug.LogWarning("[FirebaseInit] Firebase DISABLED in Editor. Will only work on device.");
-            IsInitialized = true; // Fake initialization for Editor
-            return;
-#endif
-
-            Debug.Log("[FirebaseInit] Starting Firebase initialization...");
-
-            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
-            {
-                if (task.Result != DependencyStatus.Available)
-                {
-                    Debug.LogError("Firebase dependencies missing: " + task.Result);
-                    return;
-                }
-
-                Debug.Log("Firebase Initialized");
-
-                var auth = FirebaseAuth.DefaultInstance;
-
-                auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(authTask =>
-                {
-                    if (authTask.IsFaulted)
-                    {
-                        Debug.LogError("Auth Error: " + authTask.Exception);
-                        return;
-                    }
-
-                    FirebaseUser user = auth.CurrentUser;
-
-                    if (user != null)
-                    {
-                        Debug.Log("✔ Login Anonymous OK");
-                        Debug.Log("UID: " + user.UserId);
-                        IsInitialized = true;
-                    }
-                    else
-                    {
-                        Debug.LogError("User NULL after Auth");
-                    }
-                });
             });
         }
     }
